c300fceb603eae750454daab0ad1247b1dab2fcf
Ryan Lee <ryanlee2@Ryans-Mac-Pro.local>
2021-01-04 12:35:23 -0500

    create about pg - create view all plants pg

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Plant:  # Note that parens are optional if not inheriting from another class
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+
+
+plants = [
+    Plant('Lolo', 'tabby', 'foul little demon', 3),
+    Plant('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+    Plant('Raven', 'black tripod', '3 legged cat', 4)
+]
